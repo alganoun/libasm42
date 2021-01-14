8d212c508c4222a006ef011468baff5e4988c471
Allan Ganoun <hor4tio.code@gmail.com>
2021-01-14 23:37:12 +0100

    ft_strlen add

--- ft_strlen.s
+++ ft_strlen.s
@@ -1,17 +1,15 @@
-# **************************************************************************** #
-#                                                                              #
-#                                                         :::      ::::::::    #
-#    ft_strlen.s                                        :+:      :+:    :+:    #
-#                                                     +:+ +:+         +:+      #
-#    By: alganoun <alganoun@student.42lyon.fr>      +#+  +:+       +#+         #
-#                                                 +#+#+#+#+#+   +#+            #
-#    Created: 2021/01/12 14:18:53 by alganoun          #+#    #+#              #
-#    Updated: 2021/01/12 15:14:39 by alganoun         ###   ########lyon.fr    #
-#                                                                              #
-# **************************************************************************** #
+segment .text
+		global _ft_strlen
 
-section .text
-	global _start
-_start
+_ft_strlen:
+		mov		rax, 0			; initializing a value to zero
+		jmp		count			; going to the moment the count function will loop
+	
+count:
+		cmp		[rdi=rax], 0	;  comp beetween str[rax] and 0
+		je		exit			; if the conditon is met (== 0) ten exit
+		inc		rax				; increment the value stored in rax
+		jmp		count			; go back to count function -> loop done
 
-asm -f macho64 test.s && ld -e _start -macosx_version_min 10.12 -lSystem -o test test.o
+exit:
+		ret
