8c4d1be3b033a4f6c5f4cb2e1978274e8b0c0570
hor4tio <hor4tio.code@gmail.com>
2021-01-15 15:09:24 +0100

    fixing use of parameters in the asm function

--- ft_strcmp.s
+++ ft_strcmp.s
@@ -5,11 +5,10 @@ _ft_strcmp:
 		mov		rax, 0					; initialize the return value to zero
 		jmp		comp					; going to the comp fontion
 comp:
-		mov		rdx, [rdi + rax]
-		mov		rbx, [rsi + rax]	; store the pointer soustraction in case of return
-		sub		rdx, rbx
-		cmp		rdx, 0					; compare the value of rax to zero
-		jne		exit				; if rax is different to zero call the exit function
+		mov		rdx, [rbp+4 + rax]
+		mov		rbx, [rbp+8 + rax]	; store the pointer soustraction in case of return
+		cmp		rdx, rbx			;
+		jnz		exit				; if rax is different to zero call the exit function
 		inc		rax					; increment the second argument pointer
 		jmp		comp
 exit :
